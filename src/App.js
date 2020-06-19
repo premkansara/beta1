1b104a55e02cf4a12037c7087c100bed7ba88404
Prem Kansara <pkansara97@gmail.com>
2020-06-19 09:00:23 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import MaterialTable from 'material-table';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MaterialTable
+      columns={[{title:'IP_Address',field:'IP_Address'},
+              {title: 'Status',field: 'Status'},
+              {title: 'lastSuccess',field: 'lastSuccess'},
+              {title: '2h', field: 'hour_2'},
+              {title: '8h',field: 'hour_8'},
+              {title: '1d',field: 'day_1'},
+              {title: '7d',field: 'day_7'},
+              {title: '30d',field: 'day_30'},
+              {title: 'blocks',field: 'blocks'},
+              {title: 'svcs',field: 'svcs'},
+              {title: 'version',field: 'version'}        
+              ]}
+      data={[{IP_Address: "47.90.248.68:8233", Status: "good", lastSuccess:"1591980661", hour_2: "100.00% ", hour_8: "99.95%", day_1: "94.40%", day_7: "34.30%", day_30: "9.35%"
+      ,  blocks: "864312",  services: '00000005' , version: "170010 '/MagicBean:2.1.2/'"}]}
+              />
     </div>
   );
 }
